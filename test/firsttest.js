bc4f3064b4802fc1a6e704bcce6ac74208dbb40b
Lind Alexander <Alexander.Lind@norconsult.com>
2016-10-19 11:03:09 +0200

    new test

--- test/firsttest.js
+++ test/firsttest.js
@@ -1,8 +1,17 @@
 var assert = require('assert');
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
+
+describe('MyCustomTest', function() {
+  describe('Some description', function() {
+    it('should return fail', function() {
+      assert.equal("A", "B");
     });
   });
 });
